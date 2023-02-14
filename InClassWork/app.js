4b5cc31fbedacc929c2fa33a0e14b0e5c38ec512
Jennie Zhu Pan <jenniezhupan@Jorges-MacBook-Air.local>
2023-02-13 22:02:21 -0500

    GA course day 1, recording in class work.

---
+++ InClassWork/app.js
@@ -0,0 +1,15 @@
+//Class 1//
+let myAge = 26
+let monthsInYears = 12
+let myAgeInMonths = myAge * monthsInYears
+let myFirstName = "Jennie"
+let myLastName = "Zhu"
+
+
+
+
+
+// console.log(myFirstName + ' ' + myLastName)
+// console.log(myFirstName.charAt(0) + myLastName)
+
+console.log(`${myFirstName} ${myLastName} is ${myAge}`)
